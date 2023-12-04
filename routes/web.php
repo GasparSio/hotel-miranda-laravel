ec508e64779805fa261c7d52cdaa875a48a4e1e5
GasparSio <sio.gaspar@gmail.com>
2023-12-04 17:19:09 +0100

    changes on route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OfferController;
 use App\Http\Controllers\RoomController;
 
 
-//el 'index' de dentro de la clase hace alusion al metodo del controlador.
-// Route::get('/', [IndexController::class, 'index'])->name('index');
+
 Route::get('/about', function () {
     return view('about');
 })->name('aboutus');
 
+Route::get('/offers', [OfferController::class, 'offers'])->name('offers');
 
 //agrupamos las distintas rutas que podemos tener dentro de un mismo controlador, y nos quedamos solo con la ruta y el metodo
 Route::controller(RoomController::class)->group(function () {
 
     Route::get('/', 'index')->name('index');
+    Route::get('/rooms-grid', 'rooms')->name('rooms-grid');
 });
