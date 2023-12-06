9705242d65402f20d49bd3b4b218df168dcbb202
GasparSio <sio.gaspar@gmail.com>
2023-12-06 11:27:52 +0100

    Room Model: create a static function using query checking booking available

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,9 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Room extends Model
 {
     use HasFactory;
     protected $table = 'room';
-}
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public static function available($checkin, $checkout)
+    {
+        return Room::where('status', 'Available')
+            ->where('discount', 0)
+            ->whereNotExists(
+                function ($query) use ($checkin, $checkout) {
+                    $query->select(DB::raw(1))
+                        ->from('booking as b')
+                        ->whereRaw('room.id = b.room_id')
+                        ->where(function ($subquery) use ($checkin, $checkout) {
+                            $subquery->whereBetween('b.check_in', [$checkin, $checkout])
+                                ->orWhereBetween('b.check_out', [$checkin, $checkout])
+                                ->orWhere(function ($innerSubquery) use ($checkin, $checkout) {
+                                    $innerSubquery->where('b.check_in', '>=', $checkin)
+                                        ->where('b.check_in', '<=', $checkout);
+                                })
+                                ->orWhere(function ($innerSubquery) use ($checkin, $checkout) {
+                                    $innerSubquery->where('b.check_out', '>=', $checkin)
+                                        ->where('b.check_out', '<=', $checkout);
+                                });
+                        });
+                }
+            )
+            ->get();
+    }
+};
