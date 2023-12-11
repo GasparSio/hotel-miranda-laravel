e3b34c4ee860c3d95ab732516c8c67c7001996eb
GasparSio <sio.gaspar@gmail.com>
2023-12-11 14:05:53 +0100

    added routes to different routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\OfferController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\BookingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +29,24 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/about', function () {
+    return view('about');
+})->name('aboutus');
+
+Route::get('/offers', [OfferController::class, 'index'])->name('offers');
+
+Route::controller(ContactController::class)->group(function () {
+    Route::get('/contact', 'show')->name('contact');
+    Route::post('/contact', 'store')->name('contact');
+});
+Route::controller(RoomController::class)->group(function () {
+    Route::get('/', 'show_all_rooms')->name('index');
+    Route::get('/rooms-grid', 'show_available_rooms')->name('rooms-grid');
+    Route::get('/room-detail', 'show_related_rooms')->name('room-detail');
+});
+Route::controller(BookingController::class)->group(function () {
+    Route::post('/room-detail', 'store')->name('rooms-grid');
+});
+
+
+require __DIR__ . '/auth.php';
