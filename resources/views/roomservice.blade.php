09fe1ef9afc8a7193ca4552fe9e8f654ee82b658
GasparSio <sio.gaspar@gmail.com>
2023-12-11 16:00:38 +0100

    create and modify roomservice

--- resources/views/roomservice.blade.php
+++ resources/views/roomservice.blade.php
@@ -1,16 +1,44 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Select your Order') }}
+            {{ __('Create your Order') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="room" :value="__('Room Number')" />
+                        <x-text-input placeholder="Write your room number" id="room" class="block mt-1 w-full" type="number" name="room" :value="old('room')" required autofocus />
+                        <x-input-error :messages="$errors->get('room')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <label for="type-order" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                        <select id="type-order" name="type-order" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                            <option value="" selected disabled>Select your type order</option>
+                            <option value="food">Food</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <div class="mt-4">
+                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..." required></textarea>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+
+                        <x-primary-button class="ms-3">
+                            {{ __('Make your order') }}
+                        </x-primary-button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
