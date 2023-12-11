ceaefdc58bf747c4af6623024f51b362c5730ee1
GasparSio <sio.gaspar@gmail.com>
2023-12-11 16:01:34 +0100

    changes on navigation route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -77,6 +77,11 @@
                 {{ __('Room Service') }}
             </x-responsive-nav-link>
         </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('your-orders')" :active="request()->routeIs('your-orders')">
+                {{ __('Your Orders') }}
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
