5b0ad621fedf7cbb33dde9ed24791439500839fb
GasparSio <sio.gaspar@gmail.com>
2023-12-12 15:02:29 +0100

    visual changes on roomservice

--- resources/views/roomservice.blade.php
+++ resources/views/roomservice.blade.php
@@ -12,29 +12,29 @@
                     @csrf
 
                     <!-- Email Address -->
-                    <div class="mt-4 flex flex-col ">
+                    <div style="width: 95%; margin: auto; margin-top: 10px;" class="mt-4 flex flex-col">
                         <x-input-label for="room" :value="__('Room Number')" class="m-auto w-11/12" />
                         <x-text-input placeholder="Write your room number" id="room" class="block mt-1 m-auto w-11/12" type="number" name="room_id" :value="old('room')" max="300" required autofocus />
                         <x-input-error :messages="$errors->get('room')" class="mt-2" />
                     </div>
 
                     <!-- Password -->
-                    <div class="mt-4 flex flex-col">
-                        <label for="type-order" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white m-auto w-11/12">Select an option</label>
+                    <div style="width: 95%; margin: auto; margin-top: 10px;" class="flex flex-col">
+                        <label for="type-order" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                         <select id="type-order" name="type" class="m-auto w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500" required>
                             <option value="" selected disabled>Select your type order</option>
                             <option value="food">Food</option>
                             <option value="other">Other</option>
                         </select>
                     </div>
-                    <div class="mt-4 flex flex-col">
-                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white m-auto w-11/12">Your message</label>
+                    <div style="width: 95%; margin: auto; margin-top: 10px;" class="flex flex-col">
+                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
                         <textarea id="message" rows="4" name="description" class="block text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 m-auto w-11/12" placeholder="Write your thoughts here..." required></textarea>
                     </div>
                     <input type="hidden" name="user_id" value="{{Auth::id()}}">
                     <div class="flex items-center justify-center mt-4">
 
-                        <x-primary-button class="ms-3">
+                        <x-primary-button style="margin: auto; margin-top: 10px; margin-bottom: 10px;" class="ms-3">
                             {{ __('Make your order') }}
                         </x-primary-button>
                     </div>
